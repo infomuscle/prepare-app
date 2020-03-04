705bdfbed7b506a237e7fd65bd40f2078a9b41d2
infomuscle <infomuscle10@gmail.com>
2020-03-04 20:40:17 +0900

    init prepare app

---
+++ Prepare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prepare
+//
+//  Created by 정보근 on 2020/03/04.
+//  Copyright © 2020 Bokeun Jeong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
