aa918f2f30f2943f8408684752e142867eec905b
infomuscle <infomuscle10@gmail.com>
2020-03-07 00:06:04 +0900

    scrollview added

--- Prepare/ViewController.swift
+++ Prepare/ViewController.swift
@@ -9,37 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var btnPlan: UIButton!
     
-    
+    @IBOutlet var scrollView: UIScrollView!
     var plans = Array<Array<String>>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        plans = [["Los Angeles", "bg-001.jpg"], ["Las Vegas", "bg-002.jpg"], ["San Francisco", "bg-003.jpg"]]
+        
+        
+
+        plans = [["Los Angeles", "bg-001.jpg"], ["Las Vegas", "bg-002.jpg"], ["San Francisco", "bg-003.jpg"], ["San Diegeo", "bg-004.jpg"]]
 
         let lblX: CGFloat = 16
-        var lblY: CGFloat = 121
+        var lblY: CGFloat = 15
         let btnX: CGFloat = 16
-        var btnY: CGFloat = 150
+        var btnY: CGFloat = 45
         
         if plans.isEmpty {
+            // Label
             let dynamicLbl = UILabel()
             dynamicLbl.textColor = UIColor.lightGray
             dynamicLbl.text = "Make new plan"
             dynamicLbl.frame = CGRect(x: lblX, y: lblY, width: 200, height: 21)
-            self.view.addSubview(dynamicLbl)
+            self.scrollView.addSubview(dynamicLbl)
             
+            // Button
             let dynamicBtn = PlanButton()
             dynamicBtn.borderColor = UIColor.lightGray
             dynamicBtn.borderWidth = 1
             dynamicBtn.cornerRadius = 10
+            
             dynamicBtn.setTitle("+", for: UIControl.State.normal)
             dynamicBtn.setTitleColor(UIColor.lightGray, for: UIControl.State.normal)
             dynamicBtn.frame = CGRect(x: btnX, y: btnY, width: 343, height: 161)
-            self.view.addSubview(dynamicBtn)
+            self.scrollView.addSubview(dynamicBtn)
         } else {
             
             for i in 0..<plans.count {
@@ -48,23 +51,26 @@ class ViewController: UIViewController {
                 dynamicLbl.text = plans[i][0]
                 dynamicLbl.textColor = UIColor.lightGray
                 dynamicLbl.frame = CGRect(x: lblX, y: lblY, width: 200, height: 21)
-                self.view.addSubview(dynamicLbl)
+                self.scrollView.addSubview(dynamicLbl)
                 
                 // Buttion
                 let dynamicBtn = PlanButton()
                 dynamicBtn.borderColor = UIColor.lightGray
                 dynamicBtn.borderWidth = 1
                 dynamicBtn.cornerRadius = 10
+                
                 dynamicBtn.clipsToBounds = true
                 let bgImg: UIImage? = UIImage(named: plans[i][1])
                 dynamicBtn.setBackgroundImage(bgImg, for: UIControl.State.normal)
                 dynamicBtn.frame = CGRect(x: btnX, y: btnY, width: 343, height: 161)
-                self.view.addSubview(dynamicBtn)
+                self.scrollView.addSubview(dynamicBtn)
                 
                 lblY += 210
                 btnY += 210
             }
         }
+        
+        scrollView.contentSize = CGSize(width: btnX, height: btnY)
     }
 
 }
